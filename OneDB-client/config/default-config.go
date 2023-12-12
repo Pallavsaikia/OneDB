9a84a83402bb8f07abc16bd8472b37243ff03ffa
pallav saikia <pallav.saikia@styldod.com>
2023-12-12 22:32:29 +0530

    created base structures

--- OneDB-client/config/default-config.go
+++ OneDB-client/config/default-config.go
@@ -5,7 +5,4 @@ const (
     DEFAULT_HOST="127.0.0.1"
     DEFAULT_PORT="3407"
 )
-func establishConnection(username string, database string, port string, host string,password string) {
-	// fmt.Print("Trying to connect to "+host+":"+port+" with user :"+username)
-	return
-}
+
