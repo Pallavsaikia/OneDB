88b96b1a4e25e8ddc463caf8447ff7056bcbdf9f
pallav saikia <pallav.saikia@styldod.com>
2023-12-23 16:47:06 +0530

    changes client to rust

--- OneDB-client/config/default-config.go
+++
@@ -1,8 +0,0 @@
-package config
-
-const (
-    DEFAULT_USERNAME="root"
-    DEFAULT_HOST="127.0.0.1"
-    DEFAULT_PORT="3407"
-)
-
--- OneDB-client/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/Pallavsaikia/OneDb/OneDB-client
-
-go 1.20
